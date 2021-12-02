66e2c9c68be7037a0b24ee787ab120032c046774
Quentin <qmgermain@gmail.com>
2021-12-02 09:30:09 +0100

    timer

---
+++ Timer.java
@@ -0,0 +1,31 @@
+public class Timer extends Thread
+{
+	private boolean alive;
+	private AppModel app_model;
+
+	public Timer(AppModel am)
+	{
+		this.alive = true;
+		this.app_model = am;
+	}
+
+	public void run()
+	{
+		while (this.alive) {
+			try {
+				this.sleep(1000);
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
+			if (this.alive) {
+				this.app_model.incrCounter();
+			}
+		}
+	}
+
+	public void kill()
+	{
+		this.alive = false;
+	}
+}
