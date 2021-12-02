0a311199999481150970402dccc90b2f5df1fa9e
Quentin <qmgermain@gmail.com>
2021-12-02 09:37:23 +0100

    model

---
+++ AppModel.java
@@ -0,0 +1,40 @@
+public class AppModel extends Model
+{
+	private int counter;
+	private boolean counting;
+	private Timer timer;
+
+	public AppModel()
+	{
+		this.counter = 0;
+		this.counting = false;
+		this.timer = null;
+	}
+
+	public void incrCounter()
+	{
+		if (this.counting) {
+			this.counter++;
+			this.notifyObservers();
+		}
+	}
+
+	public void stopCounting()
+	{
+		this.counting = false;
+		this.timer.kill();
+		this.timer = null;
+	}
+	public void startCounting()
+	{
+		this.counting = false;
+		this.timer = new Timer(this);
+		this.timer.start();
+	}
+
+	private void notifyObservers()
+	{
+		Object[] args = {this.counter};
+		this.notify(args);
+	}
+}
